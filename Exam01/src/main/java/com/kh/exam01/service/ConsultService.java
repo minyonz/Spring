f3c2fa78d76aa5751c1fdf4714992c5c48172bc5
minyon <als543413@gmail.com>
2021-07-06 12:08:45 +0900

    remove exam01

--- Exam01/src/main/java/com/kh/exam01/service/ConsultService.java
+++
@@ -1,9 +0,0 @@
-package com.kh.exam01.service;
-
-import java.util.List;
-
-import com.kh.exam01.domain.ConsultVo;
-
-public interface ConsultService {
-	public List<ConsultVo> listConsult(String sno);
-}
