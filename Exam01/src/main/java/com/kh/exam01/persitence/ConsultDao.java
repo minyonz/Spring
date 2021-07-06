b9bf0180ce238970a1d6c383f786196014dffaee
minyon <als543413@gmail.com>
2021-07-06 10:51:20 +0900

    exam01

--- Exam01/src/main/java/com/kh/exam01/persitence/ConsultDao.java
+++
@@ -1,9 +0,0 @@
-package com.kh.exam01.persitence;
-
-import java.util.List;
-
-import com.kh.exam01.domain.ConsultVo;
-
-public interface ConsultDao {
-	public List<ConsultVo> listConsult(String sno);
-}
